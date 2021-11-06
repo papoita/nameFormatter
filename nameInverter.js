5c8ad580eab703450f8059f2f3a831216f2803a0
PaolaP <paolaperez8@gmail.com>
2021-11-05 23:33:35 -0400

    running code

--- nameInverter.js
+++ nameInverter.js
@@ -1,14 +1,37 @@
-const nameInverter = function (name) {
-  name = name.replace(/\s/g, '');
-  if (name === '') {
-
-    return '';
+const nameInverter = function(name) {
+  let newName = "";
+  if (name === undefined) {
+    throw "Error";
   }
-  return name;
-}
-nameInverter(" name ")
-nameInverter("first last");
+  if (name) {
+    const trimmedString = name.trim();
+    const newString = trimmedString.split(" ");
+    // newName = name.replace(/\s/g, '');
+    //return newName;
+    //checking if string is single word
+    if (newString.length === 1) {
+      if (newString[0] === "Dr.") {
+        return "";
+      }
+      return newString;
+    } else if (newString.length === 2) {
+      if (newString[0] === "Dr.") {
+        return (`${newString[0]} ${newString[1]}`);
+      } else {
+        return (`${newString[1]}, ${newString[0]}`);
+      }
+    } else if (newString.length === 3) {
+      if (newString[0] === "Dr.") {
+        return (`${newString[0]} ${newString[2]}, ${newString[1]}`);
+      }
+    }
+  }
+  return newName;
+};
+//nameInverter(" name ")
+//nameInverter(" first last");
+nameInverter(" steven");
+//nameInverter("Dr. first");
 
 module.exports = nameInverter;
-
 
